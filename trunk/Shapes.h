15a185d4bd1d818a1c419926a6271667603ad1c6
arnaringib <arnaringib@cfee87e8-01ee-11df-bfb3-7781b9cb07ea>
2010-03-27 22:45:44 +0000

    Initial commit, made an Shapes class which can be used to inherit. Created two other classes hBox and hSphere. And used them both in the main function.

---
+++ trunk/Shapes.h
@@ -0,0 +1,74 @@
+//
+// Þessi abstract klasi er hugsaður sem undirstaða
+// fyrir öll önnur einföld form.
+//
+// -Arnar Ingi Bragason
+
+#ifndef _SHAPES_H
+#define _SHAPES_H
+
+#include <H3D/X3D.h>
+#include <H3D/Shape.h>
+#include <H3D/Appearance.h>
+#include <H3D/Material.h>
+#include <H3D/Transform.h>
+#include <H3D/MagneticSurface.h>
+#include <H3DUtil/Rotation.h>
+
+using namespace std;
+using namespace H3D;
+using namespace H3DUtil::ArithmeticTypes;
+
+class Shapes{
+public: 
+	Shapes();
+	virtual ~Shapes();
+
+	// Notkun: shapes->setTranslation(translation);
+	// Fyrir:  shapes er bendir á Shapes hlut, translation
+	//		   er Vec3f.
+	// Eftir:  Búið er að færa shapes hlutinn á translation((x,y,z))
+	//		   hnit miðað við (0,0,0).
+	void setTranslation(const Vec3f &translation);
+	
+	// Notkun: shapes->setColor(red,green,blue);
+	// Fyrir:  shapes er bendir á Shapes hlut, 
+	//		   red, green og blue eru float.
+	// Eftir:  shapes er teiknað í (red,green,blue) lit. 
+	void setColor(const float &red, const float &green, const float &blue);
+
+	// Notkun: shapes->setRotation(axis,angle);
+	// Fyrir:  shapes er bendir á Shapes hlut, 
+	//		   axis er Vec3f og angle er float.
+	// Eftir:  shapes er snúið við á axis og í angle gráður.
+	void setRotation(const Vec3f &axis, const float &angle);
+
+	// Notkun: shapes->setSurface(stiffness, damping, staticf, dynamicf, snap);
+	// Fyrir:  shapes er bendir á Shapes hlut,
+	//		   stiffness, damping, staticf, dynamicf og snap eru float.
+	// Eftir:  Flöturinn er með þéttleikan stiffness, damping í mjúkleika,
+	//		   núningurinn er staticf, hreyfinúningurinn er dynamicf og 
+	//		   snap er lengd frá hlutnum sem aðdráttur á að virka.
+	void setSurface(const float &stiffness, const float &damping, const float &staticf, const float &dynamicf, const float &snap);
+
+
+	// Notkun: shapes->setSolid(solid);
+	// Fyrir:  shapes er bendir á Shapes hlut, solid er bool.
+	// Eftir:  shapes er teiknað báðum megin ef og 
+	//		   aðeins ef solid er true. Upphafsgildið er true.
+	virtual void setSolid(const bool &solid)=0;
+
+	// Notkun: node = shapes->getNode();
+	// Fyrir:  shapes er bendir á Shapes hlut,
+	//		   node er Node hlutur.
+	// Eftir:  node inniheldur shapes hnút.
+	virtual Node* getNode(void)=0;
+protected:
+	Transform *transform;
+	Shape *shape;
+	Appearance *appearance;
+	Material *material;
+	MagneticSurface *surface;
+};
+
+#endif
---
+++ trunk/main.cpp
@@ -0,0 +1,46 @@
+#include <H3D/X3D.h>
+#include <H3D/Viewpoint.h>
+#include <H3D/Scene.h>
+#include <H3D/GLUTWindow.h>
+#include "hSphere.h"
+#include "hBox.h"
+
+using namespace H3D;
+
+Viewpoint *vp;
+hSphere *hsphere;
+hBox *hbox;
+AutoRef< Group > group( new Group );
+AutoRef< Scene > scene( new Scene );
+
+int main(int argc, char* argv[]) {
+	vp = new Viewpoint;
+
+	vp->position->setValue(Vec3f(0,0,0.6f));
+
+	hsphere = new hSphere();
+	hsphere->setRadius(0.1f);
+	hsphere->setSolid(true);
+	hsphere->setColor(1,0,0);
+
+	hbox = new hBox();
+	hbox->setSize(Vec3f(0.1f,0.1f,0.1f));
+	hbox->setSolid(true);
+	hbox->setColor(0,1,0);
+	hbox->setTranslation(Vec3f(0.1f,0.0f,0.0f));
+	
+	group->children->push_back(hsphere->getNode());
+	group->children->push_back(hbox->getNode());
+	
+	// create a window to display
+	GLUTWindow *glwindow = new GLUTWindow;
+
+	// add the window to the scene.
+	scene->window->push_back( glwindow );
+
+	// add our group node that is to be displayed to the scene.
+	scene->sceneRoot->setValue(group.get());
+
+	// start the main loop
+	Scene::mainLoop();
+}
