8985754a2b3f2157e01280edb10ad54b2167df8a
arnaringib <arnaringib@cfee87e8-01ee-11df-bfb3-7781b9cb07ea>
2010-04-11 17:02:19 +0000

    Added some comments on the Haptic Device class and fixed few variables name.

--- trunk/HapticDevice.h
+++ trunk/HapticDevice.h
@@ -1,3 +1,8 @@
+//
+// Þessi klasi sér um alla virkni stafsins
+// getur tekið alla þá stafi sem virka með H3D.
+//
+
 #ifndef _HATPICDEVICE_H
 #define _HATPICDEVICE_H
 
@@ -15,17 +20,53 @@ public:
 	HapticDevice();
 	~HapticDevice();
 	
+	// Notkun: hdevice->setupDevice();
+	// Fyrir:  hdevice er bendir á HapticDevice hlut,
+	//         og stafur er tengdur við tölvuna.
+	// Eftir:  Búið er tengjast stafnum 
 	void setupDevice();
+
+	// Notkun: hdevice->enableDevice();
+	// Fyrir:  hdevice er bendir á HapticDevice hlut,
+	//         og búið að tengjast stafnum.
+	// Eftir:  Búið er að virka stafinn. 
 	void enableDevice();
+
+	// Notkun: hdevice->disableDevice();
+	// Fyrir:  hdevice er bendir á HapticDevice hlut,
+	//         og búið að virkja stafinn.
+	// Eftir:  Búið er að af virkja stafinn. 
 	void disableDevice();
+
+	// Notkun: err = hdevice->getLastErrorMsg();
+	// Fyrir:  hdevice er bendir á HapticDevice hlut,
+	//         err er string.
+	// Eftir:  err inniheldur síðasta villuboði
+	//         frá stafnum.
 	string &getLastErrorMsg();
+
+	// Notkun: en = hdevice->enabled();
+	// Fyrir:  hdevice er bendir á HapticDevice hlut,
+	//         en er bool.
+	// Eftir:  en er satt ef og aðeins ef 
+	//         stafurinn er virkur, annars ósatt.
 	bool enabled();
+
+	// Notkun: hdevice->setPosition(position);
+	// Fyrir:  hdevice er bendir á HapticDevice hlut,
+	//         position er Vec3f.
+	// Eftir:  stafurinn er stilltur á position hnit.
 	void setPosition(const Vec3f &position);
+
+	// Notkun: node = hdevice->getNode();
+	// Fyrir:  hdevice er bendir á HapticDevice hlut,
+	//		   node er Node hlutur.
+	// Eftir:  node inniheldur HapticDevice hnút.
 	Node* getNode(void);
 
 private:
 	AutoRef< Node > deviceSetup;
-	string deviceinfo_file;
+	string deviceinfo_string;
 	bool enable;
 	string lastErrorMsg;
 	DeviceInfo *di;
